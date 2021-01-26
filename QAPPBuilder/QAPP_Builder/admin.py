d880fe55a1c40b077c10a40f4996858565b6a74a
Daniel L. Young, Ph.D <youngd62@outlook.com>
2021-01-26 13:11:10 -0500

    Update QAPP Builder module

---
+++ QAPPBuilder/QAPP_Builder/admin.py
@@ -0,0 +1,59 @@
+# admin.py (QAPP_Builder)
+# !/usr/bin/env python3
+# coding=utf-8
+# young.daniel@epa.gov
+
+
+"""
+Defines classes used to generate Django Admin portion of website.
+
+Should be an Admin class for each Model that can be modified by an admin user.
+
+Available functions:
+- TBD
+"""
+
+from django.contrib import admin
+from QAPP_Builder.forms import QappForm
+from QAPP_Builder.models import Qapp, SectionA, SectionB, SectionD, \
+    References, Revision, Division
+
+
+class QappAdmin(admin.ModelAdmin):
+    """
+    Define options used to display and edit Qapp objects.
+
+    (QAPP_Builder) on the Django Admin page.
+    """
+
+    list_display = ("division_branch", "title", "qa_category",
+                    "revision_number", "strap", "tracking_id")
+    search_fields = ("division_branch", "title", "qa_category")
+    list_per_page = 25
+    form = QappForm
+
+    def save_model(self, request, obj, form, change):
+        """
+        Overwrite the default save_model method.
+
+        So we can automatically set the prepared_by field as current user.
+        """
+        # Only set prepared_by when it's the first save (create)
+        if not obj.pk:
+            obj.prepared_by = request.user
+        return super().save_model(request, obj, form, change)
+
+
+admin.site.register(Qapp, QappAdmin)
+
+admin.site.register(Division)
+
+admin.site.register(SectionA)
+
+admin.site.register(SectionB)
+
+admin.site.register(SectionD)
+
+admin.site.register(References)
+
+admin.site.register(Revision)
---
+++ QAPPBuilder/QAPP_Builder/models.py
@@ -0,0 +1,63 @@
+# Local_settings.py (QAPP_Builder)
+# !/usr/bin/env python3
+# coding=utf-8
+# young.daniel@epa.gov
+
+"""
+Local Django settings for QAPP_Builder project.
+
+Available functions:
+- Settings in this file are for developer/engineer to run on localhost.
+- DO NOT CHANGE!
+"""
+
+SITE_NAME = 'https://134.67.216.106/accounts/login'
+
+DEFAULT_FROM_EMAIL = 'young.daniel@epa.gov'
+EMAIL_HOST = '134.67.216.102'
+EMAIL_HOST_USER = 'dyoung11'
+EMAIL_HOST_PASSWORD = 'Evelynj1!'
+EMAIL_PORT = 25
+EMAIL_FILE_PATH = '/var/www/html/QAPP_Builder/uploads'
+
+USER_APPROVAL_EMAIL = [
+    'young.daniel@epa.gov',
+    'meyer.david@epa.gov',
+    'gonzalez.michael@epa.gov',
+]
+
+# SECURITY WARNING: Keep the secret key used in production secret!
+SECRET_KEY = 'z&&=(=sm60$x+8asdkcgfehrgc5k^5^_6q@=-uychcf$j1a-&d53u!('
+
+# SECURITY WARNING: Do not run with debug turned on in production!
+DEBUG = True
+
+# ALLOWED_HOSTS = []
+if DEBUG is True:
+    ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.v2626umcth937.rtord.epa.gov',
+                     'https://plasticsprojects.epa.gov/qar5/', '134.67.216.106']
+else:
+    ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.v2626umcth937.rtord.epa.gov',
+                     'https://plasticsprojects.epa.gov/qar5/', '134.67.216.106']
+
+BASE_URL = 'http://127.0.0.1'
+# BASE_URL = 'https://134.67.216.106' 'v2626umcth937.rtord.epa.gov'
+# EPA RTP assigned IP. Note: Need Jake or Raghu to verify BASE_URL, this app
+# will only run on RTP RHEL not STREAMS.
+
+DJANGO_SETTINGS_MODULE = 'QAPP_Builder.settings'
+
+ROOT_URLCONF = 'QAPP_Builder.urls'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'QAPP_Builder',
+        'USER': 'postgres',
+        'PASSWORD': 'Evelynj1!',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
+
+LOGIN_REDIRECT_URL = '/dashboard/'
