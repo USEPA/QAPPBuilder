37bcf402cd9e16d15c64c62c3af89b29a74428c8
jacobgqc <jacob@gqc.com>
2023-02-08 15:00:00 -0500

    Updated pip packages to resolve Dependabot issues.

--- QAPPBuilder/QAPP_Builder/settings.py
+++ QAPPBuilder/QAPP_Builder/settings.py
@@ -17,7 +17,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import posixpath
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
